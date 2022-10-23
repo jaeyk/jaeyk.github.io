1a1f85140ec0d9d956638023ae197218c388a2dd
Jae Yeon Kim <jaeyeonkim@kdis.ac.kr>
2022-10-23 14:46:59 +0900

    update CV

--- copy_cv.sh
+++ copy_cv.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+echo "copying CV"
+
 cd .. 
 cp Downloads/CV_Jae_Yeon_Kim.pdf jaeyk.github.io/.
 cd jaeyk.github.io/
 git add .
 git commit -m "update CV"
 git push
+
+echo "pushing CV done"
+
+echo "copying resume"
+
+cd .. 
+cp create_resume/resume_Jae_Yeon_Kim.pdf jaeyk.github.io/.
+cd jaeyk.github.io/
+git add .
+git commit -m "update resume"
+git push
+
+echo "pushing resume done"
