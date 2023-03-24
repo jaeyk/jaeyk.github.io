4bce3d1cbf13ff86ce49388cc2574c2cc3b27fb1
Jae Yeon Kim <jaeyeonkim@codeforamerica.org>
2023-03-24 12:45:48 -0700

    coauthor network

--- coauthors.R
+++ coauthors.R
@@ -1,32 +1,39 @@
 if(!require(pacman)) install.packages("pacman")
 
-pacman::p_load(tidyverse, ggthemes, glue)
+pacman::p_load(tidyverse, ggthemes, glue, patchwork, ggpmisc)
 
 # Load file
 df <- read.csv("coauthors_jae_yeon_kim.csv") %>%
   as_tibble() %>%
   mutate(departmenet = trimws(department))
 
-# Correct a typo
-df$institution[str_detect(df$institution, "Phew")] <- "Pew Research Center"
-
 # Visualize
-df %>%
-  mutate(fields = if_else(str_detect(tolower(department), "political|social|snf|government|inequ|socio|policy"), 
-         "Political Science/Public Policy/Sociology/Computational Social Science", "Others")) %>%
-  mutate(fields = fct_relevel(fields, "Others", after = 2)) %>%
-  group_by(institution, fields) %>%
+affil_plot <- df %>%
+  #mutate(fields = if_else(str_detect(tolower(department), "political|social|snf|government|inequ|socio|policy|interna|org|busi|mana"), 
+   #      "Social sciences", "Others (e.g., Engineering)")) %>%
+  #mutate(fields = fct_relevel(fields, "Others (e.g., Engineering)", after = 2)) %>%
+  group_by(institution, year) %>%
   summarize(n = n()) %>%
   ungroup() %>%
-  ggplot(aes(x = fct_reorder(institution, n), y = n, fill = fields)) +
+  ggplot(aes(x = fct_reorder(institution, n), y = n, fill = year)) +
     geom_col() +
     coord_flip() +
-    ggthemes::theme_fivethirtyeight() +
     labs(x = "", y = "Count", 
          title = "Coauthors' Affiliated Institutions",
-         subtitle = "Including the coauthors of published and working papers",
+         subtitle = glue("Including the coauthors of published, \n working papers, and research proposals"),
          caption = glue("Update: {Sys.Date()}"),
-         fill = "Field") +
+         fill = "Year") +
     theme(legend.position = "bottom")
 
-ggsave("coauthors.png", width = 10, height= 10)
+year_plot <- df %>%
+  group_by(year) %>%
+  count() %>%
+  ggplot(aes(x = year, y = n)) +
+  geom_point() +
+  stat_poly_line() +
+  stat_poly_eq(aes(label = paste(after_stat(eq.label)))) +
+  labs(x = "Year", y = "# of the new coauthors")
+
+affil_plot + year_plot + plot_annotation(tag_levels = "A")
+
+ggsave("coauthors.png", width = 14, height= 10)
