df2a43ddadf8578fe0c23f223a1b48f7addefed8
jaeyk <jaeykim2@gmail.com>
2024-09-04 20:39:19 -0400

    update CV

--- copy_cv.sh
+++ copy_cv.sh
@@ -5,8 +5,3 @@ echo "copying CV"
 cd .. 
 cp ~/Downloads/CV_Jae_Yeon_Kim.pdf ~/Documents/jaeyk.github.io/.
 cd ~/Documents/jaeyk.github.io/
-git add .
-git commit -m "update CV"
-git push
-
-echo "pushing CV done"
